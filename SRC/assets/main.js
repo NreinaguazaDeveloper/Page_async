35ce6107bf846e506f3a53c65eb174f4af354c4f
Camilo Guaza <camiloguaza@gmail.com>
2023-06-10 21:34:36 -0500

    cambios generados v3

--- SRC/assets/main.js
+++ SRC/assets/main.js
@@ -1,15 +1,16 @@
-const API = 'https://youtube-v3-alternative.p.rapidapi.com/channel?id=UChgqWfxsDo3oemzgq_gXCUg';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UChgqWfxsDo3oemzgq_gXCUg&part=snippet%2Cid&order=date&maxResults=10';
 
 const content = null || document.getElementById('content');
 
 const options = {
-	method: 'GET',
-	headers: {
+  method: 'GET',
+  headers: {
 		'X-RapidAPI-Key': '9070f2194emshfa986355724fac2p18c183jsn9545129b3552',
-		'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
-async function fetchData(urlApi){
+
+async function fetchData(urlApi) {
   const response = await fetch(urlApi, options);
   const data = await response.json();
   return data;
@@ -18,32 +19,29 @@ async function fetchData(urlApi){
 (async () => {
   try {
     const videos = await fetchData(API);
-    let view = `
-    ${videos.items.map(video => `
-      <div class="group relative">
-        <div
-          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-        </div>
-        <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
-            <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
-          </h3>
-        </div>
-      </div>
-    `).slice(0,4).join('')}
-    `;
-    content.innerHTML = view;
-  } catch (error){
+
+    // Verificar si 'videos' tiene la estructura esperada
+    if (videos && videos.items && Array.isArray(videos.items)) {
+      let view = `
+        ${videos.items.map(video => `
+          <div class="group relative">
+            <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+              <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+              <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+              </h3>
+            </div>
+          </div>
+        `).slice(0, 4).join('')}
+      `;
+      content.innerHTML = view;
+    } else {
+      console.log('No se encontraron datos de video válidos');
+    }
+  } catch (error) {
     console.log(error);
   }
 })();
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
